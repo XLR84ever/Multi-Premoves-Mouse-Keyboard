3658cd776c2cba14697292e3c3c990e048a85026
mr-yt12 <35060206+mr-yt12@users.noreply.github.com>
2021-11-23 19:22:06 +0300

    Update content.js

--- Extension/content.js
+++ Extension/content.js
@@ -1134,6 +1134,7 @@ contentCode(); */
 
 const modify = () => {
   //|mskchess\.ru
+  return;
   if (
     /^https:\/\/(lichess\.org|lichess\.dev)\/(\w{8}|\w{12})(\/white|\/black)?$/.test(
       window.location.href
